8496c9045d3ac052dd025250b43e9cc7fa969d3e
1t1e1 <1t1ebir@gmail.com>
2020-09-07 12:39:04 +0300

    first draft countupt with cards

---
+++ src/Component/CountUpCard.js
@@ -0,0 +1,15 @@
+import CountUp from "react-countup";
+import React from "react";
+import { Card, CardHeader, CardText, CardTitle } from "reactstrap";
+
+export default function CountUpCard({ start, end, header }) {
+	return (
+		<Card className="border-light text-white bg-info">
+			<CardTitle> {header}</CardTitle>
+			<CardText className="mb-2">
+				<CountUp start={start} end={end} duration={0.75}></CountUp>
+			</CardText>
+		</Card>
+	);
+	// return <div>hi form c u</div>;
+}
--- src/Page/MainPage/MainPage.js
+++ src/Page/MainPage/MainPage.js
@@ -1,42 +1,23 @@
 import React from "react";
 import { Row, Col } from "reactstrap";
-import CountUp from "react-countup";
+import CountUpCard from "../../Component/CountUpCard";
 
 export default function MyComponent(prop) {
 	return (
 		<>
-			<Row className="text-center">
+			<Row className="mb-4"></Row>
+			<Row className="text-center ">
 				<Col sm="12" md="6" lg="3">
-					<CountUp
-						start={0}
-						end={160527}
-						duration={1.75}
-						separator=" "
-					></CountUp>
+					<CountUpCard start={0} end={160527} header="the title "></CountUpCard>
 				</Col>
 				<Col sm="12" md="6" lg="3">
-					<CountUp
-						start={0}
-						end={160527}
-						duration={1.75}
-						separator=" "
-					></CountUp>
+					<CountUpCard start={0} end={372184} header="header"></CountUpCard>
 				</Col>
 				<Col sm="12" md="6" lg="3">
-					<CountUp
-						start={0}
-						end={160527}
-						duration={1.75}
-						separator=" "
-					></CountUp>
+					<CountUpCard start={0} end={2814} header="confirmed"></CountUpCard>
 				</Col>
 				<Col sm="12" md="6" lg="3">
-					<CountUp
-						start={0}
-						end={160527}
-						duration={1.75}
-						separator=" "
-					></CountUp>
+					<CountUpCard start={0} end={14821} header="death"></CountUpCard>
 				</Col>
 				<Col sm="12" md="12" lg="12" className="map">
 					<p> MAP</p>
